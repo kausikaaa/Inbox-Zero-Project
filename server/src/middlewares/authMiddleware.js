6531c9ed9ea2aec27cb119e8cb9c5de50dc4cc97
kausikaaa <kauss0806@gmail.com>
2025-10-13 01:05:11 +0530

    feat(auth): add authentication middleware

---
+++ server/src/middlewares/authMiddleware.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+
+export const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) return res.status(401).json({ message: "Missing token" });
+
+  const token = authHeader.split(" ")[1]; // Bearer token-> Bearer <token>
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" });
+  }
+};
