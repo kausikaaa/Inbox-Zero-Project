37a87c3aff1eaea708eda6f29147e294d6f896f4
kausikaaa <kauss0806@gmail.com>
2025-10-24 22:49:29 +0530

    Recreate deleted pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,49 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import SignupPage from "./pages/SignupPage.jsx";
 import InboxPage from "./pages/InboxPage.jsx";
 import ArchivedPage from "./pages/ArchivedPage.jsx";
 
+// ProtectedRoute wrapper
+function ProtectedRoute({ children }) {
+  const token = localStorage.getItem("token"); // JWT from login
+  if (!token) return <Navigate to="/login" />;
+  return children;
+}
+
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/" element={<InboxPage />} />
-      <Route path="/archived" element={<ArchivedPage />} />
-    </Routes>
+    <div className="min-h-screen bg-gray-50">
+      <Navbar /> {/* Always visible */}
+      <div className="p-4">
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <InboxPage />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/archived"
+            element={
+              <ProtectedRoute>
+                <ArchivedPage />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Redirect unknown routes to inbox */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
